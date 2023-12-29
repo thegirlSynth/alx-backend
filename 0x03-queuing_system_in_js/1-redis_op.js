16cf4e8ab943fe47a285d608682dc1a108bcc261
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-29 15:22:47 +0100

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(`Error retrieving value for ${schoolName}: ${err}`);
+      return;
+    }
+    console.log(`Value for ${schoolName}: ${reply}`);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
